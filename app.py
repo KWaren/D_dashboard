114ef75ed7ffa1e34e717f0b4abe1a57cd7d79b8
KWaren <konnonulrich@gmail.com>
2025-03-30 16:10:45 +0100

    commit

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pages
-
+# Ajouter une balise meta pour empêcher l'indexation
+st.markdown(
+    '''
+    <meta name="robots" content="noindex, nofollow">
+    ''',
+    unsafe_allow_html=True
+)
 main_page = st.Page("pages/main.py", title="Acceuil", icon="🎈")
 target = st.Page("pages/target.py", title="Tri par target", icon="📈")
 page_3 = st.Page("pages/week.py", title="Tri par semaine", icon="🗓️")
